c945dd0d67b20185bd929c994adefc7ab891f33d
jp117 <john.paradise117@gmail.com>
2019-10-31 18:39:12 -0400

    last 2 days of commits.  working on formulas for analysis page

--- realestatecalc/analyzeProperty/forms.py
+++ realestatecalc/analyzeProperty/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField, SelectField, RadioField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField, SelectField, RadioField, DecimalField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Optional
 from wtforms import ValidationError
 
@@ -7,7 +7,60 @@ class RentalPropertyForm(FlaskForm):
     title = StringField('Report Title', validators=[DataRequired()])
     street = StringField('Street address', validators=[Optional()])
     city = StringField('City', validators=[Optional()])
-    state = StringField('State', validators=[Optional()])
+    state = SelectField('State', choices=[
+        (' ', ' '),
+        ('AK', 'Alaska'),
+        ('AL', 'Alabama'),
+        ('AZ', 'Arizona'),
+        ('AR', 'Arkansas'),
+        ('CA', 'California'),
+        ('CO', 'Colorado'),
+        ('CT', 'Connecticut'),
+        ('DE', 'Delaware'),
+        ('FL', 'Florida'),
+        ('GA', 'Georgia'),
+        ('HI', 'Hawaii'),
+        ('ID', 'Idaho'),
+        ('IL', 'Illinois'),
+        ('IN', 'Indiana'),
+        ('IA', 'Iowa'),
+        ('KS', 'Kansas'),
+        ('KY', 'Kentucky'),
+        ('LA', 'Louisiana'),
+        ('ME', 'Maine'),
+        ('MD', 'Maryland'),
+        ('MA', 'Massachusetts'),
+        ('MI', 'Michigan'),
+        ('MN', 'Minnesota'),
+        ('MS', 'Mississippi'),
+        ('MO', 'Missouri'),
+        ('MT', 'Montana'),
+        ('NE', 'Nebraska'),
+        ('NV', 'Nevada'),
+        ('NH', 'New Hampshire'),
+        ('NJ', 'New Jersey'),
+        ('NM', 'New Mexico'),
+        ('NY', 'New York'),
+        ('NC', 'North Carolina'),
+        ('ND', 'North Dakota'),
+        ('OH', 'Ohio'),
+        ('OK', 'Oklahoma'),
+        ('OR', 'Oregon'),
+        ('PA', 'Pennsylvania'),
+        ('RI', 'Rhode Island'),
+        ('SC', 'South Carolina'),
+        ('SD', 'South Dakota'),
+        ('TN', 'Tennessee'),
+        ('TX', 'Texas'),
+        ('UT', 'Utah'),
+        ('VT', 'Vermont'),
+        ('VA', 'Virginia'),
+        ('WA', 'Washington'),
+        ('WV', 'West Virginia'),
+        ('WI', 'Wisconsin'),
+        ('WY', 'Wyoming')
+    ],
+    validators=[Optional()])
     zipcode = IntegerField('Zipcode', validators=[Optional()])
     purchasePrice = IntegerField('Purchase Price', validators=[DataRequired()])
     improvementCosts = IntegerField('Improvement Costs', validators=[Optional()])
@@ -16,14 +69,15 @@ class RentalPropertyForm(FlaskForm):
     afterRepairValue = IntegerField('After Repair Value', validators=[Optional()])
     cashPurchase = BooleanField('All Cash Purchase?', validators=[Optional()])
     downPayment = SelectField('Down Payment (Percentage of Purchase Price)', choices=[
-        (' ',' '),('.05', '5%'),('.1', '10%'),(.15, '15%'),
-        (.2, '20%'),(.25, '25%'),(.3, '30%'),(.35, '35%'),
-        (.4, '40%'),(.45, '45%'),(.5, '50%'), (.55, '55%'),
-        (.6, '60%'),(.65, '65%'),(.7, '70%'), (.75, '75%'),
-        (.8, '80%'),(.85, '85%'),(.9, '90%'), (.95, '95%'),
-        (1, '100%')],
+        (' ',' '),('.05', '5%'),('.1', '10%'),('.15', '15%'),
+        ('.2', '20%'),('.25', '25%'),('.3', '30%'),('.35', '35%'),
+        ('.4', '40%'),('.45', '45%'),('.5', '50%'), ('.55', '55%'),
+        ('.6', '60%'),('.65', '65%'),('.7', '70%'), ('.75', '75%'),
+        ('.8', '80%'),('.85', '85%'),('.9', '90%'), ('.95', '95%'),
+        ('1', '100%')],
         validators=[Optional()])
-    interestRate = IntegerField('Interest Rate %', validators=[Optional()])
+    interestRate = DecimalField('Interest Rate %', validators=[Optional()])
+    amortizationPeriod = IntegerField('Amortization Period in years', validators=[Optional()])
     feesAndPoints = IntegerField('Other fees and points from lender', validators=[Optional()])
     payFees = RadioField('How will you pay lender fees and points?', choices=[('wrap', 'Wrap points/fees into loan'), ('oop','Pay fees/points out of pocket')], validators=[Optional()])
     interestOnly = RadioField('Intesrest Only?', choices=[('Yes', 'Yes'), ('No', 'No')], validators=[DataRequired()], default="No")
@@ -37,12 +91,12 @@ class RentalPropertyForm(FlaskForm):
     insurance = IntegerField('Monthly Insurnace', validators=[Optional()])
     propertyTax = IntegerField('Yearly Property Taxes', validators=[Optional()])
     otherExpenses = IntegerField('Other Monthly Expenses', validators=[Optional()])
-    vacancy = IntegerField('Vacancy Rate %', validators=[Optional()])
-    repairsAndMaint = IntegerField('Repairs and Maintenance %', validators=[Optional()])
-    capex = IntegerField('Capital Expenditures %', validators=[Optional()])
-    managementFees = IntegerField('Management Fee %', validators=[Optional()])
-    incomeGrowth = IntegerField('Income Growth %', validators=[Optional()])
-    appreciation = IntegerField('Property Value Appreciation %', validators=[Optional()])
-    expenseGrowth = IntegerField('Expense Growth %', validators=[Optional()])
-    salesExpenses = IntegerField('Sales Expense %', validators=[Optional()])
+    vacancy = DecimalField('Vacancy Rate %', validators=[Optional()])
+    repairsAndMaint = DecimalField('Repairs and Maintenance %', validators=[Optional()])
+    capex = DecimalField('Capital Expenditures %', validators=[Optional()])
+    managementFees = DecimalField('Management Fee %', validators=[Optional()])
+    incomeGrowth = DecimalField('Income Growth %', validators=[Optional()])
+    appreciation = DecimalField('Property Value Appreciation %', validators=[Optional()])
+    expenseGrowth = DecimalField('Expense Growth %', validators=[Optional()])
+    salesExpenses = DecimalField('Sales Expense %', validators=[Optional()])
     submit = SubmitField('Analyze Property', validators=[Optional()])
--- realestatecalc/analyzeProperty/views.py
+++ realestatecalc/analyzeProperty/views.py
@@ -3,6 +3,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 from realestatecalc.analyzeProperty.forms import RentalPropertyForm
 from realestatecalc import db
 from realestatecalc.core.models import User
+from bson.objectid import ObjectId
+from decimal import Decimal
+from realestatecalc.analyzeProperty.rentalPropAnalysis import capRate, noi, freeCashFlow, monthlyLoanPayment, monthlyLoanPaymentInterestOnly
 
 analyzeProp = Blueprint('analyzeProp', __name__)
 
@@ -11,7 +14,7 @@ analyzeProp = Blueprint('analyzeProp', __name__)
 def analyze():
     form = RentalPropertyForm()
     if form.validate_on_submit():
-        db.rentalProperty.insert({
+        x = db.rentalProperty.insert({
             'user': current_user.get_id(),
             'title': form.title.data,
             'street': form.street.data,
@@ -25,7 +28,8 @@ def analyze():
             'afterRapairValue': form.afterRepairValue.data,
             'cashPurchase': form.cashPurchase.data,
             'downPayment': form.downPayment.data,
-            'interestRate': form.interestRate.data,
+            'interestRate': str(form.interestRate.data),
+            'amortizationPeriod': form.amortizationPeriod.data,
             'feesAndPoints': form.feesAndPoints.data,
             'payFees': form.payFees.data,
             'interestOnly': form.interestOnly.data,
@@ -39,14 +43,32 @@ def analyze():
             'insurance': form.insurance.data,
             'propertyTax': form.propertyTax.data,
             'otherExpenses': form.otherExpenses.data,
-            'vacancy': form.vacancy.data,
-            'repairsAndMaint': form.repairsAndMaint.data,
-            'capex': form.capex.data,
-            'managementFees': form.managementFees.data,
-            'incomeGrowth': form.incomeGrowth.data,
-            'appreciation': form.appreciation.data,
-            'expenseGrowth': form.expenseGrowth.data,
-            'salesExpenses': form.salesExpenses.data
+            'vacancy': str(form.vacancy.data),
+            'repairsAndMaint': str(form.repairsAndMaint.data),
+            'capex': str(form.capex.data),
+            'managementFees': str(form.managementFees.data),
+            'incomeGrowth': str(form.incomeGrowth.data),
+            'appreciation': str(form.appreciation.data),
+            'expenseGrowth': str(form.expenseGrowth.data),
+            'salesExpenses': str(form.salesExpenses.data)
         })
-        return 'submitted'
-    return render_template('mainSite/analyzeproperty.html', form=form)
+        return redirect(url_for('analyzeProp.analyzedReport', analyzepropertyform_id=x))
+    return render_template('analysis/analyzepropertyform.html', form=form)
+
+@analyzeProp.route('/<analyzepropertyform_id>', methods=['GET', 'POST'])
+@login_required
+def analyzedReport(analyzepropertyform_id):
+    prop = list(db.rentalProperty.find({'_id': ObjectId(analyzepropertyform_id)}))
+    prop = prop[0]
+    title = prop['title']
+    address = "{0}, {1}, {2} {3}".format(prop['street'], prop['city'], prop['state'], prop['zipcode'])
+    purchasePrice = prop['purchasePrice']
+    grossRent = (prop['rent'] * 12 * (100 - Decimal(prop['vacancy'])) / 100) + prop['otherIncome'] * 12
+    managementFee = grossRent * Decimal(prop['managementFees']) / 100
+    grossExpenses = ((prop['electricity'] + prop['waterAndSewer'] + prop['pmi'] + 
+        prop['garbage'] + prop['hoa'] + prop['insurance'] + prop['otherExpenses']) * 12 + prop['propertyTax'] + managementFee)
+    if prop['payFees'] == 'oop':
+        financeAmount = prop['purchasePrice'] * Decimal(prop['downPayment'])
+    else:
+        financeAmount = (prop['purchasePrice'] + prop['closingCosts']) * Decimal(prop['downPayment'])
+    return render_template('analysis/analysisPage.html', title=title, address=address.title())
